b23b867efd44f8bcdd3eea77e7ec8a2d7324414a
Arseny <gamzinn.201005@gmail.com>
2026-06-29 21:22:31 +0500

    Новый контроллер

---
+++ src/main/java/ru/gamzin/weather/controller/MainController.java
@@ -0,0 +1,9 @@
+package ru.gamzin.weather.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class MainController {
+}
