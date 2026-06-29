0251f758661784f5a42111e55cd2b047c941d56e
Arseny <gamzinn.201005@gmail.com>
2026-06-29 22:29:14 +0500

    Создание нового entity, обновление зависимосткй и реализация контроллера

--- src/main/java/ru/gamzin/weather/controller/MainController.java
+++ src/main/java/ru/gamzin/weather/controller/MainController.java
@@ -1,9 +1,14 @@
 package ru.gamzin.weather.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
 public class MainController {
+    @GetMapping
+    public String mainPage(){
+        return "mainpage";
+    }
 }
---
+++ src/main/java/ru/gamzin/weather/model/User.java
@@ -0,0 +1,34 @@
+package ru.gamzin.weather.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false, unique = false)
+    private String name;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @CreationTimestamp
+    @Column(name = "create_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+}
